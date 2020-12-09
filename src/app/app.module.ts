769ba6f314b6f8fde79e5f53f048c9875091d588
Simón Rodríguez <simon.rodrigueza@udea.edu.co>
2020-12-08 23:41:25 -0500

    Validación de registro creada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import{FormsModule} from '@angular/forms';
-import{HttpClientModule} from '@angular/common/http'
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { UpdateComponent } from './update/update.component';
-
+import { InfoDialogComponent } from './info-dialog/info-dialog.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule} from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent,
     LandingpageComponent,
     SigninComponent,
     SignupComponent,
-    UpdateComponent
+    UpdateComponent,
+    InfoDialogComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule
 
   ],
   exports: [
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { InfoDialogComponent } from '../info-dialog/info-dialog.component';
 
 @Component({
   selector: 'app-signup',
@@ -9,6 +12,31 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  text = 'El usuario ya existe. Revise cédula o correo.'
+  validatorGroup = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+    ]),
+    number: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[0-9]{1,11}$/),
+    ]),
+    phone: new FormControl('',[
+      Validators.required,
+      Validators.pattern(/^-?(0|[1-9]\d*)?$/)
+    ]),
+    names: new FormControl('', [
+      Validators.required
+    ]),
+    nicknames: new FormControl('', [
+      Validators.required
+    ]),
+    password: new FormControl('',[
+      Validators.required,
+      Validators.minLength(5)
+    ]),
+  });
   user = {
     id_user: '',
     names: '',
@@ -17,10 +45,33 @@ export class SignupComponent implements OnInit {
     password: '',
     phone: ''
   };
-  constructor(private authService:AuthService,
-              private router:Router) { }
+  constructor(
+    private authService: AuthService,
+    private router:Router,
+    private readonly dialog: MatDialog) { }
 
   ngOnInit() {}
+  get primEmail() {
+    return this.validatorGroup.get('email');
+  }
+  get justNumber() {
+    return this.validatorGroup.get('number');
+  }
+
+  get names() {
+    return this.validatorGroup.get('names');
+  }
+  get nicknames() {
+    return this.validatorGroup.get('nicknames');
+  }
+
+  get password(){
+    return this.validatorGroup.get('password');
+  }
+
+  get phone(){
+    return this.validatorGroup.get('phone')
+  }
 
   onSubmit() {
     this.authService.signUp(this.user)
@@ -32,9 +83,24 @@ export class SignupComponent implements OnInit {
           console.log(res);
           //this.router.navigate(['/private']);
         },
-        err => console.log(err)
+        err => this.openDialog() //err
     )
   }
+  openDialog() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.minWidth = '350px';
+    dialogConfig.maxWidth = '600px';
+
+    dialogConfig.data = {
+      title: "Error",
+      msg: this.text,
+    };
+    this.dialog.open(InfoDialogComponent, dialogConfig).afterClosed().subscribe((success) => {
+    },
+    (e) => {
+        console.error(e);
+    });
+    }
 
 
 
