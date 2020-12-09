d2d11292c1990781797f66c4c9ca49554f27d7fb
daniela <danielaocampoo@gmail.com>
2020-12-08 23:54:28 -0500

    app.modules nuevas importaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,10 @@ import { LandingpageComponent } from './landingpage/landingpage.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { UpdateComponent } from './update/update.component';
-<<<<<<< HEAD
 
 import { AuthGuard } from './guards/auth.guard';
 import { TokenInterceptorService } from './services/token-interceptor.service';
 
-=======
 import { InfoDialogComponent } from './info-dialog/info-dialog.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -28,7 +26,6 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule} from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
->>>>>>> 769ba6f314b6f8fde79e5f53f048c9875091d588
 @NgModule({
   declarations: [
     AppComponent,
